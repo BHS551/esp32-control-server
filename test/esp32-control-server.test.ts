89f719ce018d6fa5d69ad7a0287b6f333898b2c0
Heredia, Brayham <551brayham@gmail.com>
2024-02-28 15:40:11 -0500

    Initial commit

---
+++ test/esp32-control-server.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Esp32ControlServer from '../lib/esp32-control-server-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/esp32-control-server-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Esp32ControlServer.Esp32ControlServerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
